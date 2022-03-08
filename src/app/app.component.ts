6f45c27e3973da58ae178b8904aaebc1a3aac71b
Tewari <devtewari@deloitte.com>
2022-03-08 12:31:09 +0530

    Latest changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Ecommerce-Angular';
+export class AppComponent implements OnChanges{
+  message = '';
+  isEnabled =false;
+  serverName=''
+
+  name='Devesh'
+
+  constructor(){
+    setInterval(()=>{
+      this.isEnabled=true;
+    },3000)
+
+    console.log('Constructor is the first one to be picked by Angular')
+  }
+
+  handleClick(){
+    console.log('Button clicked');
+    this.message='Server was added Successfully as '+ this.serverName;
+  }
+  
+  onUpdateInputs(event:Event){
+    console.log("Inside input change")
+      this.serverName=(<HTMLInputElement>event.target).value
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+      console.log('Ng on change implemented')
+  }
 }
